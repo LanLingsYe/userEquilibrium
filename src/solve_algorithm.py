436a7d85dd29094db00727aadee8fdf83b2e4c27
LanLing <15756690859@139.com>
2025-05-01 21:15:18 +0800

    incremental assignment done

--- src/solve_algorithm.py
+++ src/solve_algorithm.py
@@ -40,45 +40,73 @@ def data_reader():
 
 
 def capacity_restriction(routes: list):
+    constant_n = 4
+    iteration_process=""
     travel_time = [0] * Route.num_routes
     link_flow = [0] * Route.num_routes
     avg_travel_time = [0] * Route.num_routes
     avg_link_flow = [0] * Route.num_routes
 
     # Initialization. n=0
-    print("counter = 0")
+    iteration_process+="counter = 0\n"
     for iRoute in range(0, Route.num_routes):
         travel_time[iRoute] = routes[iRoute].brp_func(0)
-    print("travel time: ", travel_time)
+    iteration_process += "travel time: " + ", ".join(map(str, travel_time)) + "\n"
 
     min_index = travel_time.index(min(travel_time))
     link_flow[min_index] = Route.travel_demand
-    print("link flow: ", link_flow)
+    iteration_process += "link flow: " + ", ".join(map(str, link_flow)) + "\n\n"
     avg_travel_time = list(map(lambda x, y: x + y, avg_travel_time, travel_time))
     avg_link_flow = list(map(lambda x, y: x + y, avg_link_flow, link_flow))
 
     temp_tau = [0] * Route.num_routes
-    for counter in range(1, 4):
-        print(f"\ncounter = {counter}")
+    for counter in range(1, constant_n):
+        iteration_process += f"counter = {counter}\n"
         for iRoute in range(0, Route.num_routes):
             temp_tau[iRoute] = routes[iRoute].brp_func(link_flow[iRoute])
-        print("tempt tau: ", temp_tau)
+        iteration_process += "tempt tau: " + ", ".join(map(str, temp_tau)) + "\n"
         travel_time = list(map(lambda x, y: round(0.75 * x + 0.25 * y, 1), travel_time, temp_tau))
-        print("travel time: ", travel_time)
+        iteration_process += "travel time: " + ", ".join(map(str, travel_time)) + "\n"
 
         link_flow[min_index] = 0
         min_index = travel_time.index(min(travel_time))
         link_flow[min_index] = Route.travel_demand
-        print("link flow: ", link_flow)
+        iteration_process += "link flow: " + ", ".join(map(str, link_flow)) + "\n\n"
         avg_travel_time = list(map(lambda x, y: x + y, avg_travel_time, travel_time))
         avg_link_flow = list(map(lambda x, y: x + y, avg_link_flow, link_flow))
 
-    print("\naverage")
+    iteration_process += "average\n"
     avg_link_flow = list(map(lambda x: x / 4, avg_link_flow))
     for iRoute in range(0, Route.num_routes):
         avg_travel_time[iRoute] = routes[iRoute].brp_func(avg_link_flow[iRoute])
-    print("link flow: ", avg_link_flow)
-    print("travel time: ", avg_travel_time)
+    iteration_process += "link flow: " + ", ".join(map(str, link_flow)) + "\n"
+    iteration_process += "travel time: " + ", ".join(map(str, travel_time)) + "\n"
+    path = Path('../result/capacity_restriction.txt')
+    path.write_text(iteration_process)
+
+
+def incremental_assignment(routes: list):
+    constant_n = 4
+    iteration_process = ""
+    portion = Route.travel_demand / constant_n
+    travel_time = [0] * Route.num_routes
+    link_flow = [0] * Route.num_routes
+
+    for counter in range(1, constant_n + 1):
+        iteration_process += f"counter = {counter}\n"
+        for iRoute in range(0, Route.num_routes):
+            travel_time[iRoute] = routes[iRoute].brp_func(link_flow[iRoute])
+        min_index = travel_time.index(min(travel_time))
+        link_flow[min_index] += portion
+
+        iteration_process += "travel time: " + ", ".join(map(str, travel_time)) + "\n"
+        iteration_process += "link flow: " + ", ".join(map(str, link_flow)) + "\n\n"
+
+    for iRoute in range(0, Route.num_routes):
+        travel_time[iRoute] = routes[iRoute].brp_func(link_flow[iRoute])
+    iteration_process += "travel time at convergence: " + ", ".join(map(str, travel_time)) + "\n"
+    path = Path('../result/incremental_assignment.txt')
+    path.write_text(iteration_process)
 
 
 def solve_ue():
